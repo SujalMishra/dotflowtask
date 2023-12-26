ab7ac34005d5278f9221556d00c420e395478ed5
SujalMishra <98722988+SujalMishra@users.noreply.github.com>
2023-12-26 22:30:30 +0530

    cse error res

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+
 import Navbar from './components/Navbar.js'
 import SearchBar from './components/Searchbar.js'
 import Footer from './components/Footer.js'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+  }, [])
   return (
     <>
      <Navbar />
